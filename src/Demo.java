c278afaaec591f37a1aea88e803ba93e8f39481b
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-03-16 15:31:57 +0100

    First commit

--- src/Demo.java
+++ src/Demo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -12,21 +14,57 @@ public class Demo {
         System.out.println(name);
 
         //if and else
-        if(a==8){
+        if (a == 8) {
             System.out.println("it's 8");
-    } else {
+        } else {
             System.out.println("u r wrong");
         }
 
-        //count
-        for (int i = 0; i < 10; i++) {
-            System.out.println("loops :D");
-        }
 
         //if and else (important: pay attention to all brackets are beeing used corectly)
-        if((a>0) && (a<7)){
+        if ((a > 0) && (a < 7)) {
             System.out.println("a is between 0 and 7");
         } else {
             System.out.println("it isn't");
         }
-}}
+
+        //switch  default = every other value which we didn't state with "case"
+        int u = 45;
+
+        switch (u) {
+            case 0:
+                System.out.println("is 0");
+                break;
+
+            case 1:
+                System.out.println("is 1");
+                break;
+            default:
+                System.out.println("unknown value");
+        }
+
+        //for loop    (Don't repeat yourself)
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " loop");
+        }
+
+        for (int y = 0; y < 10; y = y +2) {
+            System.out.println(y + " skip");
+
+        }
+
+
+        //while loop and random number
+
+        Random random = new Random();
+        boolean isFinished = false;
+
+        while (! isFinished){
+            int randomValue = random.nextInt(  100);
+            System.out.println("while loop");
+        }
+
+
+
+    }
+}
