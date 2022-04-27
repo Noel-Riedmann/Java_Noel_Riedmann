12eb39651bb5ab200857fe20962e7b078cb49b4e
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-04-27 15:49:39 +0200

    array_uebung_2 finnished

--- src/array_uebung_2.java
+++ src/array_uebung_2.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class array_uebung_2 {
     public static void main(String[] args) {
+
+        int[] randomNum = new int[50];
+        Random random = new Random();
+        int genNum = 0;
+        int count = 0;
+
+        for (int i = 0; i < randomNum.length; i++) {
+            genNum = random.nextInt(101);
+            randomNum[i] = genNum;
+            count =  count + genNum;
+            System.out.println(genNum);
+        }
+
+        System.out.println(count);
+
 
     }
 }
