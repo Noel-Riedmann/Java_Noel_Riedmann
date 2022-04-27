ca7229436ab98557ed0422840eb8d94af1ab99ad
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-04-27 15:23:27 +0200

    Aufgabe 1 fertig.

---
+++ src/array_uebung_1.java
@@ -0,0 +1,15 @@
+public class array_uebung_1 {
+    public static void main(String[] args) {
+
+        int[] data = {1,2,3,4,5,6,7,8,9,10};
+        int tmp;
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            tmp = data[i];
+            count = count + tmp;
+            System.out.println(tmp);
+        }
+        System.out.println(count);
+    }
+}
