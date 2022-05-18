20e0d71d5d62b00bb7e9bdb638ff2f4df048c824
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-05-18 15:15:07 +0200

    caesar übung encrypt

---
+++ src/caesar.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class caesar {
+    private static String string;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type text to encrypt now!");
+        String text = scanner.nextLine();
+        int shift = 2;
+        String caesarText = "";
+        char alphabet;
+
+        for (int i = 0; i < text.length(); i++) {
+            alphabet = text.charAt(i);
+            if (alphabet >= 'a' && alphabet <= 'z') {
+
+                alphabet = (char) (alphabet + shift );
+
+                if (alphabet > 'z') {
+
+                    alphabet = (char) (alphabet + 'a' - 'z');
+                }
+                string;
+            }
+
+
+            else if (alphabet >= 'A' && alphabet <= 'Z') {
+                alphabet = (char) (alphabet + shift);
+
+
+                if (alphabet > 'Z') {
+
+                    alphabet = (char)
+                            (alphabet + 'A' - 'Z');
+                }
+                caesarText = caesarText + alphabet;
+            } else {
+                caesarText = caesarText + alphabet;
+            }
+
+        }
+        System.out.println("Encryption: " + caesarText);
+    }
+}
+
+
