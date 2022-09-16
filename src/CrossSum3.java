d5e2dc7a74468e78cd7ecc14d37dfabb2ce0747d
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-09-16 14:48:25 +0200

    CrossSum 4 not quiet finished!

--- src/CrossSum3.java
+++ src/CrossSum3.java
@@ -1,16 +1,31 @@
 public class CrossSum3 {
     public static void main(String[] args) {
-        for (int i = 0; i < 1001; i++) {
-            String numberInString = Integer.toString(i);
+        for (int y = 0; y < 1001; y++) {
+            String numberInString = Integer.toString(y);
             char[] numbers = numberInString.toCharArray();
             int sum = 0;
             for (int j = 0; j < numbers.length; j++) {
                 sum = sum + Character.getNumericValue(numbers[j]);
 
+
+            int [] fr = new int [numbers.length];
+            int visited = -1;
+            for(int i = 0; i < numbers.length; i++){
+                int count = 1;
+                for(int x = i+1; x < numbers.length; x++){
+                    if(numbers[i] == numbers[x]){
+                        count++;
+                        fr[x] = visited;
+                    }
+                }
+                if(fr[i] != visited)
+                    fr[i] = count;
             }
+            for(int i = 0; i < fr.length; i++){
+                if(fr[i] != visited)
+                    System.out.println("num " + numbers[i] + " " + "frq " + fr[i]);
 
 
-
-        }
+        }}
     }
-}
+}}
---
+++ src/CrossSum4.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.concurrent.LinkedTransferQueue;
+
+public class CrossSum4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean whileTrue = true;
+        System.out.println("Press 1 to Start and 2 to End");
+        while (whileTrue) {
+            int selection = scan.nextInt();
+
+            // Get input and convert it to array
+            if (selection == 1) {
+
+                boolean whileTrue2 = true;
+                while (whileTrue2){
+                System.out.println("Type number: ");
+                int selectionCrossSum = scan.nextInt();
+                String numberInString = Integer.toString(selectionCrossSum);
+                char[] numbers = numberInString.toCharArray();
+
+                //set sum to 0 and calc crosssum
+                int sum = 0;
+                for (int i = 0; i < numbers.length; i++) {
+                    sum = sum + Character.getNumericValue(numbers[i]);
+
+                }
+
+                //Print value and text to play again
+                System.out.println(sum);
+
+                if (sum >= 10) {
+                    selectionCrossSum = sum;
+
+                }
+                if (sum < 10) {
+                    System.out.println("Press 1 to play again or 2 to stop!");
+                }
+            }}}
+
+            //End boolean to end the program
+            if (selection == 2) {
+                whileTrue = false;
+            }
+        }
+    }
+}
