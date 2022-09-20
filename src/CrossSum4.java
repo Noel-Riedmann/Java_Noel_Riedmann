c3c310e821faaf4f342ce9d74554beccaff3fd3a
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2022-09-20 10:14:01 +0200

    CrossSum4 - finished

--- src/CrossSum4.java
+++ src/CrossSum4.java
@@ -15,34 +15,36 @@ public class CrossSum4 {
                 boolean whileTrue2 = true;
                 System.out.println("Type number: ");
                 int selectionCrossSum = scan.nextInt();
-                while (whileTrue2){
-                String numberInString = Integer.toString(selectionCrossSum);
-                char[] numbers = numberInString.toCharArray();
+                while (whileTrue2) {
+                    String numberInString = Integer.toString(selectionCrossSum);
+                    char[] numbers = numberInString.toCharArray();
 
-                //set sum to 0 and calc crosssum
-                int sum = 0;
-                for (int i = 0; i < numbers.length; i++) {
-                    sum = sum + Character.getNumericValue(numbers[i]);
+                    //set sum to 0 and calc crosssum
+                    int sum = 0;
+                    for (int i = 0; i < numbers.length; i++) {
+                        sum = sum + Character.getNumericValue(numbers[i]);
 
+                    }
+
+                    //Print value and text to play again
+                    System.out.println(sum);
+
+                    if (sum >= 10) {
+                        selectionCrossSum = sum;
+
+                    }
+                    if (sum < 10) {
+                        System.out.println("Press 1 to play again or 2 to stop!");
+                        whileTrue2 = false;
+                    }
                 }
-
-                //Print value and text to play again
-                System.out.println(sum);
-
-                if (sum >= 10) {
-                    selectionCrossSum = sum;
-
-                }
-                if (sum < 10) {
-                    System.out.println("Press 1 to play again or 2 to stop!");
-                    whileTrue2 = false;
-                }
-            }}
+            }
 
             //End boolean to end the program
             if (selection == 2) {
                 whileTrue = false;
-            }}
+            }
         }
     }
+}
 
