ea0fc0f4ee8f4d20cbc98ac7a66bc7451823a20a
Noel Riedmann <noel.riedmann@student.htldornbirn.at>
2023-01-10 11:30:26 +0100

    finished basic calculation types with saved results

--- src/at/noel/OO/calculator/BasicMath.java
+++ src/at/noel/OO/calculator/BasicMath.java
@@ -3,72 +3,105 @@ package at.noel.OO.calculator;
 import java.util.Scanner;
 
 public class BasicMath {
-    private double result;
+    private double result = 0.0;
 
     public BasicMath() {
     }
     private Scanner scanner = new Scanner(System.in);
 
-    public void start() {
-        System.out.println("|--------------------------Calculator--------------------------|");
-        System.out.println("-Press 1 to do basic calculation types");
-        System.out.println("-Press 2 to do sinus or cosinus");
-        System.out.println("-Press 3 to do square roots");
-        System.out.println("-Press 9 to exit");
-        int userInput = scanner.nextInt();
-
-        switch(userInput){
-            case 1:
-                basicCalculationTypes();
-                break;
-            case 2:
-                System.out.println("NA yet");
-                break;
-            case 3:
-                System.out.println("NA yet");
-                break;
-            case 9:
-                break;
-        }
-    }
-    public void basicCalculationTypes(){
+    public void start(){
+        //introduction
         System.out.println("|--------------------------Calculator--------------------------|");
         System.out.println("Press + or - or * or / to choose your type of calculation");
         String userChoice = scanner.next();
-        switch (userChoice){
-            case "+":
-                System.out.print("Enter 1st value: ");
-                double addValue1 = scanner.nextDouble();
-                System.out.print("Enter 2nd value: ");
-                double addValue2 = scanner.nextDouble();
-                result = addValue1 + addValue2;
-                System.out.println(addValue1 + " + " + addValue2 + " = " + result);
-                break;
-            case "-":
-                System.out.print("Enter 1st value: ");
-                double subValue1 = scanner.nextDouble();
-                System.out.print("Enter 2nd value: ");
-                double subValue2 = scanner.nextDouble();
-                result = subValue1 - subValue2;
-                System.out.println(subValue1 + " - " + subValue2 + " = " + result);
-                break;
-            case "*":
-                System.out.print("Enter 1st value: ");
-                double mulValue1 = scanner.nextDouble();
-                System.out.print("Enter 2nd value: ");
-                double mulValue2 = scanner.nextDouble();
-                result = mulValue1 * mulValue2;
-                System.out.println(mulValue1 + " * " + mulValue2 + " = " + result);
-                break;
-            case "/":
-                System.out.print("Enter 1st value: ");
-                double divValue1 = scanner.nextDouble();
-                System.out.print("Enter 2nd value: ");
-                double divValue2 = scanner.nextDouble();
-                result = divValue1 / divValue2;
-                double roundedResult = Math.round(result*100.0)/100.0;
-                System.out.println(divValue1 + " / " + divValue2 + " = " + roundedResult);
-                break;
+        //all possible calculations
+        if (userChoice.equalsIgnoreCase("+") || userChoice.equalsIgnoreCase("-") || userChoice.equalsIgnoreCase("*") || userChoice.equalsIgnoreCase("/")){
+            switch (userChoice){
+                case "+":
+                    double addValue1;
+                    if (result == 0.0) {
+                        System.out.print("Enter 1st value: ");
+                        addValue1 = scanner.nextDouble();
+                    }
+                    else {
+                        addValue1 = result;
+                        System.out.println("1st Value equals " + addValue1);
+                    }
+                    System.out.print("Enter 2nd value: ");
+                    double addValue2 = scanner.nextDouble();
+                    result = addValue1 + addValue2;
+                    System.out.println(addValue1 + " + " + addValue2 + " = " + result);
+                    break;
+                case "-":
+                    double subValue1;
+                    if (result == 0.0) {
+                        System.out.print("Enter 1st value: ");
+                        subValue1 = scanner.nextDouble();
+                    }
+                    else {
+                        subValue1 = result;
+                        System.out.println("1st Value equals " + subValue1);
+                    }
+                    System.out.print("Enter 2nd value: ");
+                    double subValue2 = scanner.nextDouble();
+                    result = subValue1 - subValue2;
+                    System.out.println(subValue1 + " - " + subValue2 + " = " + result);
+                    break;
+                case "*":
+                    double mulValue1;
+                    if (result == 0.0) {
+                        System.out.print("Enter 1st value: ");
+                        mulValue1 = scanner.nextDouble();
+                    }
+                    else {
+                        mulValue1 = result;
+                        System.out.println("1st Value equals " + mulValue1);
+                    }
+                    System.out.print("Enter 2nd value: ");
+                    double mulValue2 = scanner.nextDouble();
+                    result = mulValue1 * mulValue2;
+                    System.out.println(mulValue1 + " * " + mulValue2 + " = " + result);
+                    break;
+                case "/":
+                    double divValue1;
+                    if (result == 0.0) {
+                        System.out.print("Enter 1st value: ");
+                        divValue1 = scanner.nextDouble();
+                    }
+                    else {
+                        divValue1 = result;
+                        System.out.println("1st Value equals " + divValue1);
+                    }
+                    System.out.print("Enter 2nd value: ");
+                    double divValue2 = scanner.nextDouble();
+                    result = divValue1 / divValue2;
+                    double roundedResult = Math.round(result*100.0)/100.0;
+                    System.out.println(divValue1 + " / " + divValue2 + " = " + roundedResult);
+                    break;
+            }
+        }
+        else {
+            System.out.println("Invalid Input!");
+            start();
+        }
+        //ask user to continue
+        System.out.println("Do you want to continue with saved result (" + result + ") [Y/N]");
+        String userContinue = scanner.next();
+        if (userContinue.equalsIgnoreCase("y")){
+            start();
+        }
+        else if (userContinue.equalsIgnoreCase("n")){
+            System.out.println("Do you want to continue without saved result else the code will be exited [Y/N]");
+            userContinue = scanner.next();
+            if (userContinue.equalsIgnoreCase("y")){
+                result = 0.0;
+                start();
+            }
+            else {
+                System.out.println("Goodbye");
+                System.exit(69);
+
+            }
         }
     }
 
--- src/at/noel/OO/calculator/Main.java
+++ src/at/noel/OO/calculator/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         BasicMath basicMath = new BasicMath();
         basicMath.start();
 
